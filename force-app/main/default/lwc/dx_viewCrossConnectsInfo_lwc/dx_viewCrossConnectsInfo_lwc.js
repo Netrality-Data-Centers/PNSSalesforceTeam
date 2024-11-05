a63eff3af3ae64fa60868ea4ae3b84339ee9f582
Cristian Ruiz <cristianivan.ruiz@salesforce.com>
2024-11-05 00:18:44 +0000

    Fixing Cross Connects Fields

--- force-app/main/default/lwc/dx_viewCrossConnectsInfo_lwc/dx_viewCrossConnectsInfo_lwc.js
+++ force-app/main/default/lwc/dx_viewCrossConnectsInfo_lwc/dx_viewCrossConnectsInfo_lwc.js
@@ -2,8 +2,8 @@ import { LightningElement, wire } from 'lwc';
 import getCrossConnets from "@salesforce/apex/DX_viewCrossConnectsInformation_ctr.getCrossConnets";
 
 const columns = [
-    { label: 'Cross Connect ID', fieldName: 'Xconnect_ID__c' },
-    { label: 'Building', fieldName: 'Building__c'},
+    { label: 'Cross Connect ID', fieldName: 'Name' },
+    { label: 'Building', fieldName: 'Building_Name'},
     { label: 'Origination Account', fieldName: 'Origination_Account_Name'},
     { label: 'Destination Account', fieldName: 'Destination_Account_Name'},
     { label: 'Cross Connect Status', fieldName: 'Xconnect_Status__c'}
@@ -18,12 +18,18 @@ export default class Dx_viewCrossConnectsInfo_lwc extends LightningElement {
     wiredCrossConnetsInfo({ error, data }) {
         if (data) {
             if(data.length > 0){
-                var result = JSON.parse(JSON.stringify(data));
-                for(var item in result){
-                    var Destination_Account_Name = result[item].Destination_Account__r.Name;
-                    var Origination_Account_Name = result[item].Origination_Account__r.Name;
+                let result = JSON.parse(JSON.stringify(data));
+                let Destination_Account_Name;
+                let Origination_Account_Name;
+                let Building_Name;
+                for(let item in result){
+                    Destination_Account_Name = result[item].Destination_Account__r.Name;
+                    Origination_Account_Name = result[item].Origination_Account__r.Name;
                     result[item].Origination_Account_Name = Origination_Account_Name;
                     result[item].Destination_Account_Name = Destination_Account_Name;
+
+                    Building_Name = result[item].Building__r.Name;
+                    result[item].Building_Name = Building_Name;
                 }
                 this.dataInfo = [...result];
             }
