f90d987ed8954141745050d25916ecd49aca7691
Alberto Juarez <ajuarez@salesforce.com>
2024-11-07 23:28:14 +0000

    Sprint 2 - Open Service Requests LWC

---
+++ force-app/main/default/lwc/dx_viewOpenServiceRequest_lwc/dx_viewOpenServiceRequest_lwc.js
@@ -0,0 +1,94 @@
+import { LightningElement, wire, api } from 'lwc';
+import getOpenServiceRequest from "@salesforce/apex/DX_viewOpenServiceRequest_ctr.getOpenServiceRequest";
+//import { NavigationMixin } from 'lightning/navigation';
+import dx_URL_Experience_Cloud_lbl from '@salesforce/label/c.DX_URL_Experience_Cloud'; 
+import dx_NamePage_DetailsSR_Experience_Cloud_lbl from '@salesforce/label/c.DX_NamePage_DetailsSR_Experience_Cloud'; 
+import dx_NamePage_EditSR_Experience_Cloud_lbl from '@salesforce/label/c.DX_NamePage_EditSR_Experience_Cloud'; 
+
+const actions = [
+    { label: 'Edit', name: 'edit' },
+    { label: 'View Details', name: 'show_details' }
+];
+
+const columns = [
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions },
+    },
+    { label: 'SR Number', fieldName: 'CaseNumber' },
+    { label: 'Category', fieldName: 'RecordType_Name'},
+    { label: 'Type', fieldName: 'Type'},
+    { label: 'Status', fieldName: 'Status'},
+    { label: 'Site', fieldName: 'DX_Site__c'},
+    { label: 'SR Created Date', fieldName: 'CreatedDate'}
+];
+
+export default class Dx_viewOpenServiceRequest_lwc extends LightningElement{
+    dataInfo = [];
+    columns = columns;
+    isEmpty = false;
+    error = false;
+    @api recordIdToFlow;
+    isModalOpen = false;
+
+    @wire(getOpenServiceRequest)
+    wiredOpenSR({ error, data }) {
+        if (data) {
+            if(data.length > 0){
+                let result = JSON.parse(JSON.stringify(data));
+                let recordType_Name;
+                let isoDate;
+                let date;
+                let year;
+                let month;
+                let day;
+                let formattedDate;
+                for(var item in result){
+                    recordType_Name = result[item].RecordType.Name;
+                    result[item].RecordType_Name = recordType_Name;
+
+                    isoDate = result[item].CreatedDate;
+                    date = new Date(isoDate);
+                    // Get date components
+                    year = date.getUTCFullYear();
+                    month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+                    day = String(date.getUTCDate()).padStart(2, '0');
+                    // Format the date
+                    formattedDate = `${year}-${month}-${day}`;
+                    result[item].CreatedDate = formattedDate;
+
+                }
+                this.dataInfo = [...result];
+            }            
+            if(data.length === 0){this.isEmpty = true;}
+        } else if (error) {
+            this.error = true;            
+            console.error(error);
+        }
+    }
+
+    handleRowAction(event) {
+        const actionName = event.detail.action.name;
+        const row = event.detail.row;
+        switch (actionName) {
+            case 'edit':
+                this.EditRow(row);
+                break;
+            case 'show_details':
+                this.showRowDetails(row);
+                break;
+        }
+    }
+
+    EditRow(row) {
+        const url = dx_URL_Experience_Cloud_lbl + dx_NamePage_EditSR_Experience_Cloud_lbl + `recordIdToFlow=${row.Id}`;  
+        console.log(url);
+        window.location.href = url;
+    }
+
+    showRowDetails(row) {
+        const url = dx_URL_Experience_Cloud_lbl + dx_NamePage_DetailsSR_Experience_Cloud_lbl + `recordIdToFlow=${row.Id}`;  
+        console.log(url);
+        window.location.href = url;
+    }
+}
